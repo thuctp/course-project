921e256e39a799a1c57a3ce235b54e20d2c06c4f
Arsen Truong <Arsen.Truong@singlife.com>
2023-04-12 12:32:42 +0700

    add component

---
+++ src/app/components/admin/manage-course/manage-course.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-course',
+  templateUrl: './manage-course.component.html',
+  styleUrls: ['./manage-course.component.scss']
+})
+export class ManageCourseComponent {
+
+}
---
+++ src/app/components/card-course/card-course.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-course',
+  templateUrl: './card-course.component.html',
+  styleUrls: ['./card-course.component.scss']
+})
+export class CardCourseComponent {
+
+}
