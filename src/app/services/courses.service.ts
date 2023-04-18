f35f986199296cc110cdb47f7deb6e38515ffd43
Arsen Truong <Arsen.Truong@singlife.com>
2023-04-19 04:38:31 +0700

    func add edit remove course

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Subject, of } from 'rxjs';
 import { delay, tap } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 export type Course = {
-  id: number,
+  id: string,
   name: string,
   image: string,
   author: string,
@@ -21,7 +22,7 @@ export class CoursesService {
 
   course: Subject<any> = new Subject();
 
-  constructor() {
+  constructor(private router: Router) {
     // setInterval(() => {
     //   this.course.next(this._listCourse[Math.round(Math.random()* 2)])
     // }, 2000)
@@ -29,7 +30,7 @@ export class CoursesService {
 
   private _listCourse: Course[] = [
     {
-      id: 1,
+      id: "1",
       name: "The Complete 2023 Web Development BootcampBecome",
       image: "default.png",
       author: "Alex",
@@ -39,7 +40,7 @@ export class CoursesService {
       description: "Web Development BootcampBecome a Full-Stack Web Developer with just ONE course. HTML, CSS, Javascript, Node, React, MongoDB, Web3."
     },
     {
-      id: 2,
+      id: "2",
       name: "100 Days of Code: The Complete Python Pro Bootcamp for 2023",
       image: "default.png",
       author: "Alex",
@@ -49,7 +50,7 @@ export class CoursesService {
       description: "Master Python by building 100 projects in 100 days. Learn data science, automation, build websites, games."
     },
     {
-      id: 3,
+      id: "3",
       name: "Ultimate AWS Certified Solutions Architect Associate SAA-C03Full",
       image: "default.png",
       author: "Alex",
@@ -59,7 +60,6 @@ export class CoursesService {
       description: " Practice Exam"
     }
   ]
-
 
   getNormalCourses(){
     return ([...this._listCourse])
@@ -77,8 +77,24 @@ export class CoursesService {
 
   pushToProduct(item: any) {
     this._listCourse.push(item);
-    alert("Add New Course Successfully")
+    alert("Add New Course Successfully " + item.name);
+    this.router.navigate(['/admin']);
     console.log(this._listCourse);
 
+  }
+
+  updateToCourse(item: any) {
+    const indexToUpdate = this._listCourse.findIndex(fItem => fItem.id === item.id);
+    this._listCourse[indexToUpdate] = item;
+    alert("Update Course Successfully " + item.name);
+    console.log(this._listCourse);
+  }
+
+  deleteCourse(item: any){
+    if (confirm('Are you sure you want to delete this item?')) {
+      const indexToDelete = this._listCourse.findIndex(fItem => fItem.id === item);
+      this._listCourse.splice(indexToDelete, 1);
+      this.router.navigate(['/admin']);
+    }
   }
 }
